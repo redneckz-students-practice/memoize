49f7f4e08e6ce5594428590ced9e3ba438179864
Aleksei Solodov <Aleksei_Solodov@epam.com>
2017-12-12 22:27:36 +0300

    implemented memoize function

--- src/memoize.js
+++ src/memoize.js
@@ -1,3 +1,29 @@
-export function memoize() {
-    // TODO
+export function memoize(func) {
+    if (typeof func !== 'function') {
+        return null;
+    }
+
+    const cache = {};
+
+    return function memoizedFunc(...rest) {
+        let index = 0;
+        let returnResult;
+        if (rest.length > 0) {
+            index = getHash(rest);
+        }
+
+        if (!(index in cache)) {
+            returnResult = func.apply(this, rest);
+            cache[index] = returnResult;
+        }
+        return cache[index];
+    };
+}
+
+function getHash(args) {
+    let hash = '';
+    for (let i = 0; i < args.length; i += 1) {
+        hash += JSON.stringify(args[i]);
+    }
+    return hash;
 }
