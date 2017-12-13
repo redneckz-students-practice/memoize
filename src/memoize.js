0030674c76e786e38b621ae2bd4586c38f7bc67c
Denis Semenikhin <denis_semenikhin@epam.com>
2017-12-13 17:44:07 +0300

    done

--- src/memoize.js
+++ src/memoize.js
@@ -1,3 +1,25 @@
-export function memoize() {
-    // TODO
+export function memoize(func) {
+    if (typeof func !== 'function') {
+        return null;
+    }
+    const cache = {};
+
+    return function memoizedFunc(...rest) {
+        let index = 0;
+        let returnResult;
+        if (rest.length > 0) {
+            index = getHash(rest);
+        }
+
+        if (!(index in cache)) {
+            returnResult = func.apply(this, rest);
+            cache[index] = returnResult;
+        }
+        return cache[index];
+    };
+}
+
+function getHash(args) {
+    return args.reduce((previousValue, currentValue) =>
+        previousValue + JSON.stringify(currentValue));
 }
