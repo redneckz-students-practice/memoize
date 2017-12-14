3bac1b582b79e32c764d4b3511200cf4bf857c21
Aleksei Solodov <Aleksei_Solodov@epam.com>
2017-12-14 18:11:32 +0300

    refactoring

--- src/memoize.js
+++ src/memoize.js
@@ -3,24 +3,47 @@ export function memoize(func) {
         return null;
     }
 
-    const cache = {};
+    const cache = new Map();
 
     return function memoizedFunc(...rest) {
-        let index = 0;
-        let returnResult;
-        if (rest.length > 0) {
-            index = getHash(rest);
-        }
+        const index = getHash(rest);
 
-        if (!(index in cache)) {
-            returnResult = func.apply(this, rest);
-            cache[index] = returnResult;
+        if (!cache.has(index)) {
+            cache.set(index, func.apply(this, rest));
         }
-        return cache[index];
+        return cache.get(index);
     };
 }
 
-function getHash(args) {
-    return args.reduce((previousValue, currentValue) =>
-        previousValue + JSON.stringify(currentValue));
+function getHash(obj) {
+    let hash = '';
+
+    switch (getType(obj)) {
+        case 'Number': hash += `Number ${obj}`;
+            break;
+        case 'String': hash += `String ${obj}`;
+            break;
+        case 'Boolean': hash += `Boolean ${obj}`;
+            break;
+        case 'Array':
+            hash += 'Array[';
+            obj.forEach((elem) => {
+                hash += getHash(elem);
+            });
+            hash += ']';
+            break;
+        case 'Object':
+            hash += 'Object{';
+            Object.keys(obj).forEach((prop) => {
+                hash += getHash(prop);
+            });
+            hash += '}';
+            break;
+        default: hash += 'null';
+    }
+    return hash;
+}
+
+function getType(obj) {
+    return Object.prototype.toString.call(obj).slice(8, -1);
 }
