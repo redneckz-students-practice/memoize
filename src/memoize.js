870294f879a362743fb1b46b8511f18e6874807f
Mikhail Shirenkov <Mikhail_Shirenkov@epam.com>
2017-12-14 11:24:01 +0300

    Done

--- src/memoize.js
+++ src/memoize.js
@@ -1,3 +1,28 @@
-export function memoize() {
-    // TODO
+export function memoize(func) {
+    if (typeof func !== 'function') {
+        return null;
+    }
+
+    const cache = [];
+
+    return function memoizedFunc(...rest) {
+        let index = 0;
+        let result;
+        if (rest.length !== 0) {
+            index = getHash(rest);
+        }
+
+        if (!(index in cache)) {
+            result = func.apply(this, rest);
+            cache[index] = result;
+        }
+        return cache[index];
+    };
+}
+
+function getHash(rest) {
+    function appendCurrent(previousValue, currentValue) {
+        return previousValue + JSON.stringify(currentValue);
+    }
+    return rest.reduce(appendCurrent);
 }
