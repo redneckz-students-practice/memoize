49859adbfe3db6a7520c5440c5de7a3c801178c1
Daria Aleksandrova <Daria_Aleksandrova@epam.com>
2017-12-21 14:51:15 +0300

    task memoize

--- src/memoize.js
+++ src/memoize.js
@@ -1,3 +1,17 @@
-export function memoize() {
-    // TODO
+export function memoize(func) {
+    if (typeof func !== 'function') {
+        return null;
+    }
+    const cache = new Map();
+    return function memoizedFunc(...args) {
+        const key = getHash(args);
+        if (!(cache.has(key))) {
+            cache.set(key, func.apply(this, args));
+        }
+        return cache.get(key);
+    };
+}
+
+function getHash(args) {
+    return JSON.stringify(args);
 }
