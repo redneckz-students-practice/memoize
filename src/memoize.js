77d32183da5a8d07e303cbc737c3413b8b84b897
Aleksandr Bochkov <aleksandr_bochkov@epam.com>
2018-10-01 10:53:24 +0300

    add memoize function https://jirapct.epam.com/jira/browse/EPMFARMJTC-3507

--- src/memoize.js
+++ src/memoize.js
@@ -1,3 +1,15 @@
-export function memoize() {
-    // TODO
+export function memoize(func) {
+    if (!(typeof func === 'function')) return null;
+    const storage = new Map();
+    function generateKey(args) {
+        const key = args.map(elem => `${(typeof elem)}:${elem}`).join('|');
+        return key;
+    }
+    return function (...args) {
+        const key = generateKey(args);
+        if (storage.has(key)) return storage.get(key);
+        const value = func.apply(this, args);
+        storage.set(key, value);
+        return value;
+    };
 }
