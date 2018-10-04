5b965a9e15e832e66ca788c260897c3c8264e67d
Aleksandr Bochkov <aleksandr_bochkov@epam.com>
2018-10-04 10:41:08 +0300

    add tests for object arguments

--- src/memoize.js
+++ src/memoize.js
@@ -1,15 +1,3 @@
 export function memoize(func) {
-    if (!(typeof func === 'function')) return undefined;
-    const storage = new Map();
-    function generateKey(args) {
-        const key = args.map(elem => `${(typeof elem)}:${elem}`).join('|');
-        return key;
-    }
-    return function (...args) {
-        const key = generateKey(args);
-        if (storage.has(key)) return storage.get(key);
-        const value = func.apply(this, args);
-        storage.set(key, value);
-        return value;
-    };
+    // TODO
 }
